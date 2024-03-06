e4fcb8e8f155380d4d0533871ebc10071c32add0
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-06 14:15:16 +0530

    [MODIFIED]:Ritesh The Calculation Of Montly Win Or Loose

--- src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
@@ -8,50 +8,62 @@ public class GamblingSimulation {
 	private static final int PER_GAME_BET=1;
 	private static final int WIN=1;
 	private static final int LOOSE=0;
-	private static int winStack=0;
-	private static int looseStack=0;
+	private static final int[] winAmount = new int[20];
+	private static final int[] looseAmount = new int[20];
+	
 	static int playStatus;
 	
 	public static void gameStatus() {
 		
 		System.out.println("Every Day Stack = "+PER_DAY_STACK);
 		System.out.println("Per Game Bet = "+PER_GAME_BET);
-		System.out.println("Loose Game : "+looseStack);
-		System.out.println("Win Game :"+winStack);
-		System.out.println("-------------");
+		
 	}
 	
 	public static void gamePlay() {
 		
 		Random random=new Random();
 		
-		while(true) {
-			gameStatus();
-			playStatus=random.nextInt(9)%2;
+		for(int i = 0;i <20;i++) {
 			
-			if(playStatus == LOOSE) {
+			 System.out.println("day :"+(i+1));
+			 int winStack=0;
+			 int looseStack=0;
+			
+			while(true) {
 				
-				looseStack += 1;
+				playStatus=random.nextInt(9)%2;
+				
+				if(playStatus == LOOSE) {
+					
+					looseStack += 1;
+				}
+				else {
+					winStack += 1;
+				}
+				
+				if(winStack == PER_DAY_STACK / 2 ) {
+					break;
+				}
+				if( looseStack == PER_DAY_STACK / 2) {
+					break;
+				}	
 			}
-			else {
-				winStack += 1;
-			}
+			winAmount[i]=winStack;
+			looseAmount[i]=looseStack;
 			
-			if(winStack == PER_DAY_STACK / 2 ) {
-				break;
-			}
-			if( looseStack == PER_DAY_STACK / 2) {
-				break;
-			}	
+			System.out.println("Loose Game : "+looseStack);
+			System.out.println("Win Game :"+winStack);
+			System.out.println("-------------");	
 			
-		 }
-	  }
+		}
+	
+	 }
     
 	public static void main(String[] args) {
 		
 	System.out.println("Wellcome To Gambling Simulation Program develop by Ritesh Badgujar");
 	
-	gameStatus();
 	gamePlay();
 
 	}
