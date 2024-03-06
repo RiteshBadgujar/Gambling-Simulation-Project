0f6280aaaa0ed4f78c97b04e2b1fdfb36c88d152
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-06 09:19:05 +0530

    [ADDED]:Ritesh- Gambling Simulation Setup

---
+++ src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
@@ -0,0 +1,11 @@
+package com.ritesh.gamblingsimulationproblem;
+
+public class GamblingSimulation {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Wellcome To Gambling Simulation Program ");
+	
+	}
+
+}
