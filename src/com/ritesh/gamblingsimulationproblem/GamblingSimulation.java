efc7a3e07631f34b395409ac2f32bf6d635ee3ef
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-06 09:41:37 +0530

    [MODIFIED]:Ritesh UC-1 Setting Every Day Stack And Per Game Bet

--- src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
@@ -1,11 +1,22 @@
 package com.ritesh.gamblingsimulationproblem;
 
 public class GamblingSimulation {
-
+	
+	private static final int EVERY_DAY_STACK=100;
+	private static final int PER_GAME_BET=1;
+	
+	public static void gameStatus() {
+		
+		System.out.println("Every Day Stack = "+EVERY_DAY_STACK);
+		System.out.println("Per Game Bet = "+PER_GAME_BET);
+	}
+	
 	public static void main(String[] args) {
 		
 	System.out.println("Wellcome To Gambling Simulation Program ");
 	
+	gameStatus();
+	
 	}
-
+	
 }
