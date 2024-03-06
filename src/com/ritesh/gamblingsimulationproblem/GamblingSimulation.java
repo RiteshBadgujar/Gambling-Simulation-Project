d9805805f90f509d07bd1adc91fcb26ea3afe976
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-06 12:57:54 +0530

    [MODIFIED]:Ritesh Win or Loose 50% then Resign Game

--- src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class GamblingSimulation {
 	
-	private static final int EVERY_DAY_STACK=100;
+	private static final int PER_DAY_STACK=100;
 	private static final int PER_GAME_BET=1;
 	private static final int WIN=1;
 	private static final int LOOSE=0;
@@ -14,36 +14,46 @@ public class GamblingSimulation {
 	
 	public static void gameStatus() {
 		
-		System.out.println("Every Day Stack = "+EVERY_DAY_STACK);
+		System.out.println("Every Day Stack = "+PER_DAY_STACK);
 		System.out.println("Per Game Bet = "+PER_GAME_BET);
 		System.out.println("Loose Game : "+looseStack);
 		System.out.println("Win Game :"+winStack);
 		System.out.println("-------------");
-		
 	}
 	
 	public static void gamePlay() {
 		
 		Random random=new Random();
-		playStatus=random.nextInt(9)%2;
 		
-		if(playStatus == LOOSE) {
+		while(true) {
+			gameStatus();
+			playStatus=random.nextInt(9)%2;
 			
-			looseStack+=1;
-		}
-		else {
-			winStack+=1;
-		}
-	
-	}
-	
+			if(playStatus == LOOSE) {
+				
+				looseStack += 1;
+			}
+			else {
+				winStack += 1;
+			}
+			
+			if(winStack == PER_DAY_STACK / 2 ) {
+				break;
+			}
+			if( looseStack == PER_DAY_STACK / 2) {
+				break;
+			}	
+			
+		 }
+	  }
+    
 	public static void main(String[] args) {
 		
 	System.out.println("Wellcome To Gambling Simulation Program develop by Ritesh Badgujar");
 	
 	gameStatus();
 	gamePlay();
-	gameStatus();
+
 	}
 	
 }
