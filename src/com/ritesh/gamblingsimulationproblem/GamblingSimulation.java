5534ef766612ba884beee5e76053284fee38dac1
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-03-09 13:52:54 +0530

    [ADDED]:Ritesh Check For Lucky and Unlucky day

--- src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
+++ src/com/ritesh/gamblingsimulationproblem/GamblingSimulation.java
@@ -11,6 +11,14 @@ public class GamblingSimulation {
 	
 	private static final int[] winAmount = new int[20];
 	private static final int[] looseAmount = new int[20];
+	private static int[] maxwin=new int[20];
+	private static int[] maxloose=new int[20];
+	private static int winmax=0;
+	private static int losemax=0;
+	
+	private static int winindex=0;
+	private static int loseindex=0;
+	
 
 	static int playStatus;
 	
@@ -29,7 +37,7 @@ public class GamblingSimulation {
 		
 		for(int i = 0;i <20;i++) {
 			
-			 System.out.println("day :"+(i+1));
+			 System.out.println("Day :"+(i+1));
 			 int winStack=0;
 			 int looseStack=0;
 			
@@ -47,22 +55,44 @@ public class GamblingSimulation {
 				
 				if(winStack == PER_DAY_STACK / 2 ) {
 					total_win_day += 1;
+					
+				if(looseStack < winStack) {
+					maxwin[i]=winStack - looseStack;
+					if(winmax <maxwin[i]) {
+						winmax=maxwin[i];
+						winindex= i+1;
+					}
+						break;
+					}
 					break;
 				}
 				if( looseStack == PER_DAY_STACK / 2) {
 					total_loose_day += 1;
+					
+					if(losemax  < looseStack) {
+						
+						 maxloose[i] = looseStack - winStack;
+						if(losemax < maxloose[i]) {
+							losemax=maxloose[i];
+							loseindex= i+1;
+							break;
+					 }
+					}
+					
 					break;
-				}	
+				   }	
 			}
 			
 			winAmount[i]=winStack;
 			looseAmount[i]=looseStack;
 			
-			System.out.println("Loose Game : "+looseStack);
 			System.out.println("Win Game : "+winStack);
+			System.out.println("Loose Game : "+looseStack);
 			System.out.println("-------------");	
 			
 		}
+		System.out.println(loseindex+" Unlucky day , loss by Rs. : "+losemax);
+		System.out.println(winindex +" Lucky day  & win By RS.  : "+winmax);
 		System.out.println("Total Montly Win Day : "+total_win_day);
 		System.out.println("Total Montly Loose Day : "+total_loose_day);
 	 }
